03532259d9325ca57369009bfac637130457230e
EvanJannot <ejannot@ensc.fr>
2025-08-28 21:06:11 -0400

    Comment Tech Exploration section waiting to complete it

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -47,7 +47,7 @@ export default function AboutMe() {
 
             <IndustryInvolvmentSection title="Industry Involvment" />
 
-            <TechExplorationSection title="Tech Exploration" />
+            {/* <TechExplorationSection title="Tech Exploration" /> */}
           </div>
         </div>
       </div>
